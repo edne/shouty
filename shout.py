ab4018bc87f37a65a10d729a7103638ff1fba67f
Edoardo Negri <edne@gmx.com>
2017-03-31 20:53:51 +0200

    Read file

--- shout.py
+++ shout.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from sys import argv
 import ctypes
 import ctypes.util
 
@@ -32,21 +33,21 @@ lib.shout_set_user(obj, b'source')
 lib.shout_set_password.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
 lib.shout_set_password(obj, b'hackme')
 
-# protocol - default http
 SHOUT_PROTOCOL_HTTP = 0
 SHOUT_PROTOCOL_XAUDIOCAST = 1
 SHOUT_PROTOCOL_ICY = 2
 SHOUT_PROTOCOL_ROARAUDIO = 3
 
+# protocol - default http
 lib.shout_set_protocol.argtypes = [ctypes.c_void_p, ctypes.c_int]
 lib.shout_set_protocol(obj, SHOUT_PROTOCOL_HTTP)
 
-# format - default ogg
 SHOUT_FORMAT_OGG = 0
 SHOUT_FORMAT_MP3 = 1
 SHOUT_FORMAT_WEBM = 2
 SHOUT_FORMAT_WEBMAUDIO = 3
 
+# format - default ogg
 lib.shout_set_format.argtypes = [ctypes.c_void_p, ctypes.c_int]
 lib.shout_set_format(obj, SHOUT_FORMAT_MP3)
 
@@ -65,6 +66,24 @@ lib.shout_set_format(obj, SHOUT_FORMAT_MP3)
 lib.shout_open.argtypes = [ctypes.c_void_p]
 lib.shout_open(obj)
 # TODO: get error code
+
+
+lib.shout_send.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_size_t]
+lib.shout_sync.argtypes = [ctypes.c_void_p]
+lib.shout_close.argtypes = [ctypes.c_void_p]
+
+for file_name in argv[1:]:
+    print(file_name)
+    with open(file_name, 'rb') as f:
+        while True:
+            chunk = f.read(4096)
+            if not chunk:
+                break
+
+            lib.shout_send(obj, chunk, len(chunk))
+            lib.shout_sync(obj)
+
+        lib.shout_close(obj)
 
 lib.shout_free.argtypes = [ctypes.c_void_p]
 lib.shout_free(obj)
