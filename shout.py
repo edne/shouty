bffc45ea0e490c7efa3ec83f3d3f19ae0cbab755
Edoardo Negri <edne@gmx.com>
2017-03-31 10:27:15 +0200

    Set some parameters

--- shout.py
+++ shout.py
@@ -16,8 +16,39 @@ lib.shout_new.restype = ctypes.c_void_p
 obj = lib.shout_new()
 
 
+# host - default localhost
+host = 'localhost'
+lib.shout_set_host.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+lib.shout_set_host(obj, host.encode('ascii'))
+
+# port - default 8000
 lib.shout_set_port.argtypes = [ctypes.c_void_p, ctypes.c_int]
 lib.shout_set_port(obj, 8000)
+
+# user - default source
+lib.shout_set_user.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+lib.shout_set_user(obj, b'source')
+
+# password - NO default, must be set
+lib.shout_set_password.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+lib.shout_set_password(obj, b'hackme')
+
+# TODO (re)define constants
+
+# protocol - default http
+# format - default vorbis
+
+# mount - SHOUT_PROTOCOL_ICY doesn't support it
+# dumpfile
+# agent
+
+# Directory parameters, all optional:
+# public
+# name
+# url
+# genre
+# description
+# audio_info
 
 lib.shout_free.argtypes = [ctypes.c_void_p]
 lib.shout_free(obj)
