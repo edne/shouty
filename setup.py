3abfb766e69fd40f581dc36bb22bd3f3506c51dd
Edoardo Negri <edne@gmx.com>
2017-04-02 14:38:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='shouty',
+    version='0.1.dev1',
+
+    description='Minimal and pythonic wrapper to libshout2',
+    url='https://github.com/edne/shouty',
+
+    author='Edoardo Negri',
+    author_email='edne@gmx.com',
+
+    packages=['shouty'],
+    license='MIT',
+)
